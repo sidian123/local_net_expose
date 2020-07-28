5f7eed50f89116b124e9c048fae60a0a437183ee
sidian <sidian123@qq.com>
2020-07-29 00:32:17 +0800

    feat: 抽象并抽离<通道>类

--- local_net_expose_test/src/main/java/live/sidian/local_net_expose_test/LocalNetExposeTestApplication.java
+++ local_net_expose_test/src/main/java/live/sidian/local_net_expose_test/LocalNetExposeTestApplication.java
@@ -1,16 +1,10 @@
 package live.sidian.local_net_expose_test;
 
-import cn.hutool.core.thread.ThreadUtil;
-import cn.hutool.core.util.ArrayUtil;
-import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -30,37 +24,7 @@ public class LocalNetExposeTestApplication implements CommandLineRunner {
             Socket pythonSocket = new Socket("127.0.0.1", 8002);
 
             log.info("建立一条连接");
-            Socket finalSocket = socket;
-            ThreadUtil.execute(() -> {
-                try {
-                    byte[] bytes = new byte[1024];
-                    int len = -1;
-                    InputStream inputStream = finalSocket.getInputStream();
-                    OutputStream outputStream = pythonSocket.getOutputStream();
-                    while ((len = inputStream.read(bytes)) != -1) {
-                        outputStream.write(bytes, 0, len);
-                        System.out.println(StrUtil.str(ArrayUtil.sub(bytes, 0, len), "utf-8"));
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                log.info("o -> p close");
-            });
-            ThreadUtil.execute(() -> {
-                try {
-                    byte[] bytes = new byte[1024];
-                    int len = -1;
-                    InputStream inputStream = pythonSocket.getInputStream();
-                    OutputStream outputStream = finalSocket.getOutputStream();
-                    while ((len = inputStream.read(bytes)) != -1) {
-                        outputStream.write(bytes, 0, len);
-                        System.out.println(StrUtil.str(ArrayUtil.sub(bytes, 0, len), "utf-8"));
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                log.info("p -> o close");
-            });
+            TransmitChannel transmitChannel = new TransmitChannel(socket, pythonSocket);
         }
     }
 
